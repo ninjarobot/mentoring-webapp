da4e2baa95dce0d36d4f06fc8d5b24dc844e4e8c
Dave Curylo <dave@curylo.org>
2019-09-23 23:03:12 -0400

    Composition and Results

--- Program.fs
+++ Program.fs
@@ -11,6 +11,26 @@ type Yo =
     {
         Word : string
     }
+    member this.SpeakAndRespond () =
+        Console.WriteLine this.Word
+        let newYo = { this with Word = "hello" }
+        newYo
+
+let speakAndRespond (yo:Yo) =
+    Console.WriteLine yo.Word
+    let newYo = { yo with Word = "hello" }
+    newYo
+
+let speak (yo:Yo) =
+    Console.WriteLine yo.Word
+    yo
+
+let respond (yo:Yo) =
+    let newYo = { yo with Word = "hello" }
+    newYo
+
+let speakAndRespond2 = speak >> respond
+
 
 let simpleHandler =
     fun (next:HttpFunc) (ctx:HttpContext) ->
@@ -34,12 +54,57 @@ let configureServices (services : IServiceCollection) =
     // Add Giraffe dependencies
     services.AddGiraffe() |> ignore
 
+// Exception - you didn't expect them (network failure, disk problem, authentication issue)
+// C#, Base Class Libraries (BCL) - try/catch/finally
+
+// Error - things went wrong that you planned for
+
+let rng = Random ()
+let doSomethingThatMightFail () =
+    if rng.Next () % 2 = 0 then
+        failwith "random failure"
+    else
+        "it worked fine"
+
+let trySomethingThatMightFail () =
+    try
+        doSomethingThatMightFail ()
+        |> Result.Ok
+    with
+    | ex ->
+        System.Console.Error.WriteLine "it failed"
+        Result.Error "it failed"
+
+let showErrorIfHappened result =
+    match result with
+    | Ok success -> Ok success
+    | Error err ->
+        eprintfn "An error happened: %s" err
+        Error err
+
+let showErrorIfHappenedConcise = Result.mapError (fun err ->
+        eprintfn "An error happened: %s" err
+        err
+    )
+
+let printResultOfSomething result =
+    match result with
+    | Ok success -> printfn "Result: %s" success
+    | Error failure -> eprintfn "Failed: %s" failure
+    
+let doAndPrint = trySomethingThatMightFail >> showErrorIfHappenedConcise >> printResultOfSomething
+
 [<EntryPoint>]
 let main _ =
+    while true do
+        doAndPrint () 
+        Console.ReadLine () |> ignore
+    (*
     WebHostBuilder()
         .UseKestrel()
         .Configure(Action<IApplicationBuilder> configureApp)
         .ConfigureServices(configureServices)
         .Build()
         .Run()
+    *)
     0
